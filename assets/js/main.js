582433cbb295c8294c63bca6feb3b13761ac5291
fahim06 <fahim.yusuf06@gmail.com>
2022-01-27 03:34:12 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,53 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal =function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb,i)=>{
+    mb.addEventListener('click', ()=>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click',()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup('.work_container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
+/* Link active work */
+const LinkWork=document.querySelectorAll('.work__item')
 
-/* Link active work */ 
+function activeWork(){
+    LinkWork.forEach(l=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
 
+LinkWork.forEach(l=>l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
@@ -16,7 +55,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
